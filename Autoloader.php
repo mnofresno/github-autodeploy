d57106c6157b23520085aa4ff1f535aab1b7f51b
Mariano Fresno <mnofresno@gmail.com>
2024-04-27 00:22:16 -0300

    Fix broken tests

--- Autoloader.php
+++ Autoloader.php
@@ -1,7 +1,7 @@
 <?php
 
 class Autoloader {
-    const NAMESPACE = 'Mariano\GitAutoDeploy';
+    const NAMESPACE = 'Mariano\\GitAutoDeploy';
     const SRC_PATH = 'src';
 
     static function load() {
