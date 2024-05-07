89896d71bf25cdda353b7c34369553e2a0124fe1
Mariano Fresno <mnofresno@gmail.com>
2024-05-07 00:29:29 -0300

    Add mocked config to hamster smoke

--- test/HamsterSmokeTest.php
+++ test/HamsterSmokeTest.php
@@ -3,13 +3,17 @@
 namespace Mariano\GitAutoDeploy\Test;
 
 use DI\Container;
+use Mariano\GitAutoDeploy\ConfigReader;
 use Mariano\GitAutoDeploy\ContainerProvider;
 use Mariano\GitAutoDeploy\Hamster;
 use PHPUnit\Framework\TestCase;
 
 class HamsterSmokeTest extends TestCase {
     public function testHamsterConstruct(): void {
-        $subject = $this->getContainer()->get(Hamster::class);
+        $mockConfig = $this->createMock(ConfigReader::class);
+        $container = $this->getContainer();
+        $container->set(ConfigReader::class, $mockConfig);
+        $subject = $container->get(Hamster::class);
         $this->assertNotNull($subject);
     }
 
