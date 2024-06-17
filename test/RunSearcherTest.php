b3b1c03051a68ff64eb3d19cf2dd5641b17b5633
Mariano Fresno <mnofresno@gmail.com>
2024-06-17 02:39:43 -0300

    (fix) Broken tests used to read inexistent config

--- test/RunSearcherTest.php
+++ test/RunSearcherTest.php
@@ -7,8 +7,18 @@ use Mariano\GitAutoDeploy\RunSearcher;
 use PHPUnit\Framework\TestCase;
 
 class RunSearcherTest extends TestCase {
+    private $mockConfig;
+
+    public function setUp(): void {
+        $this->mockConfig = $this->createMock(ConfigReader::class);
+        $this->mockConfig
+            ->method('get')
+            ->with(ConfigReader::EXPOSE_RAW_LOG)
+            ->willReturn(false);
+    }
+
     public function testSearchValidNotFound(): void {
-        $subject = new RunSearcher(new ConfigReader());
+        $subject = new RunSearcher($this->mockConfig);
         $result = $subject->search('8b7f5891-04e0-4216-b299-314965940b96');
         $this->assertEquals([], $result);
     }
@@ -16,14 +26,14 @@ class RunSearcherTest extends TestCase {
     public function testSearchFoundInvalidUUID(): void {
         $testRunId = 'this-is_not_valid-uuid';
         file_put_contents(__DIR__ . '/../deploy-log.log',  \date('Y-m-d H:i:s') . ' - {"runId":"9b7f5891-04e0-4216-b299-314965940b96"}');
-        $subject = new RunSearcher(new ConfigReader());
+        $subject = new RunSearcher($this->mockConfig);
         $result = $subject->search($testRunId);
         $this->assertEquals([], $result);
     }
 
     public function testSearchFoundOldFormatTrap(): void {
         $testRunId = '7826dedf-52a5-4170-8d4f-ac6170f7df1d';
-        $subject = new RunSearcher(new ConfigReader(), __DIR__ . DIRECTORY_SEPARATOR . 'RunSearcherLogExample.txt');
+        $subject = new RunSearcher($this->mockConfig, __DIR__ . DIRECTORY_SEPARATOR . 'RunSearcherLogExample.txt');
         $result = $subject->search($testRunId);
         $this->assertEquals([
             [
@@ -60,7 +70,7 @@ class RunSearcherTest extends TestCase {
     public function testSearchFoundWithOldFormat(): void {
         $testRunId = '9b7f5891-04e0-4216-b299-314965940b96';
         file_put_contents(__DIR__ . '/../deploy-log.log',  ($givenDate = date('Y-m-d H:i:s')) . ' - {"runId":"9b7f5891-04e0-4216-b299-314965940b96"}');
-        $subject = new RunSearcher(new ConfigReader());
+        $subject = new RunSearcher($this->mockConfig);
         $result = $subject->search($testRunId);
         $this->assertEquals([
             [
@@ -76,7 +86,7 @@ class RunSearcherTest extends TestCase {
     public function testSearchFoundMonolog(): void {
         $line = '[2024-05-08T03:09:04.454598+00:00] github-autodeploy.INFO: Ran 4 commands {"context":{"runId":"1a781415-67f7-4b1c-a8ed-c240bcdf66f7","repo":"pepe_project"}} []';
         file_put_contents(__DIR__ . '/../deploy-log.log', $line);
-        $subject = new RunSearcher(new ConfigReader());
+        $subject = new RunSearcher($this->mockConfig);
         $result = $subject->search('1a781415-67f7-4b1c-a8ed-c240bcdf66f7');
         $this->assertEquals([
             [
